a41cc95e1e6cbe6004cfd3d479018d9ac3fde1ad
raphunteks <ksanihtest@gmail.com>
2026-06-05 18:17:16 +0800

    Update trigger.js

--- api/trigger.js
+++ api/trigger.js
@@ -1,6 +1,8 @@
 let needsRefresh = false;
 let pendingNotification = "";
-let nextScrapeTime = 0; // JEMBATAN WAKTU AUTO-SCRAPE REAL-TIME
+let nextScrapeTime = 0;
+let isScraping = false;
+let lastUpdateStr = "";
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -12,14 +14,15 @@ module.exports = async (req, res) => {
     if (req.method === 'POST') {
         if (req.body.refresh !== undefined) needsRefresh = req.body.refresh;
         if (req.body.notify !== undefined) pendingNotification = req.body.notify;
-        if (req.body.nextScrapeTime !== undefined) nextScrapeTime = req.body.nextScrapeTime; // Simpan waktu dari Ekstensi
-        return res.status(200).json({ status: true, needsRefresh, notify: pendingNotification, nextScrapeTime });
+        if (req.body.nextScrapeTime !== undefined) nextScrapeTime = req.body.nextScrapeTime;
+        if (req.body.isScraping !== undefined) isScraping = req.body.isScraping;
+        if (req.body.lastUpdateStr !== undefined) lastUpdateStr = req.body.lastUpdateStr;
+        return res.status(200).json({ status: true, needsRefresh, notify: pendingNotification, nextScrapeTime, isScraping, lastUpdateStr });
     }
 
     if (req.method === 'GET') {
         let currentNotif = pendingNotification;
-        pendingNotification = ""; // Kosongkan setelah dibaca oleh Bot WA
-        // Kembalikan waktu ke Website Vercel
-        return res.status(200).json({ needsRefresh, notify: currentNotif, nextScrapeTime });
+        pendingNotification = ""; 
+        return res.status(200).json({ needsRefresh, notify: currentNotif, nextScrapeTime, isScraping, lastUpdateStr });
     }
 };
