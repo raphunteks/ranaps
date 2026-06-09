a622e0060f59f923ff7533f43857a3c3d434a85b
raphunteks <ksanihtest@gmail.com>
2026-06-09 14:30:13 +0800

    Update trigger.js

--- api/trigger.js
+++ api/trigger.js
@@ -2,7 +2,19 @@ let needsRefresh = false;
 let pendingNotification = "";
 let nextScrapeTime = 0;
 let isScraping = false;
-let lastUpdateStr = "";
+
+// Menyimpan Memori Update masing-masing 9 layanan Poliklinik
+let updates = {
+    lastUpdateStrRanap: "",
+    lastUpdateStrRajalAntrianEndo: "",
+    lastUpdateStrRajalRiwayatEndo: "",
+    lastUpdateStrRajalAntrianBM: "",
+    lastUpdateStrRajalRiwayatBM: "",
+    lastUpdateStrRajalAntrianPerio: "",
+    lastUpdateStrRajalRiwayatPerio: "",
+    lastUpdateStrRajalAntrianUmum: "",
+    lastUpdateStrRajalRiwayatUmum: ""
+};
 
 module.exports = async (req, res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -16,13 +28,20 @@ module.exports = async (req, res) => {
         if (req.body.notify !== undefined) pendingNotification = req.body.notify;
         if (req.body.nextScrapeTime !== undefined) nextScrapeTime = req.body.nextScrapeTime;
         if (req.body.isScraping !== undefined) isScraping = req.body.isScraping;
-        if (req.body.lastUpdateStr !== undefined) lastUpdateStr = req.body.lastUpdateStr;
-        return res.status(200).json({ status: true, needsRefresh, notify: pendingNotification, nextScrapeTime, isScraping, lastUpdateStr });
+        
+        // Membaca dan menyimpan setiap Update Timestamp spesifik per Endpoint
+        Object.keys(updates).forEach(key => {
+            if (req.body[key] !== undefined) {
+                updates[key] = req.body[key];
+            }
+        });
+
+        return res.status(200).json({ status: true, needsRefresh, notify: pendingNotification, nextScrapeTime, isScraping, updates });
     }
 
     if (req.method === 'GET') {
         let currentNotif = pendingNotification;
         pendingNotification = ""; 
-        return res.status(200).json({ needsRefresh, notify: currentNotif, nextScrapeTime, isScraping, lastUpdateStr });
+        return res.status(200).json({ needsRefresh, notify: currentNotif, nextScrapeTime, isScraping, updates });
     }
 };
